b75eb35b7188fd2b7bd8084616eb00dcaa6d70ed
Sandeep Varma <49518103+sndp19@users.noreply.github.com>
2022-08-17 17:15:49 +0530

    Add files via upload

---
+++ B01.LevelDetection/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LevelDetection.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LevelDetection.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=leveldetection.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/leveldetection.x.tar
